1967862c3d77fdd2fe2119a4585b650e6f6aa0dc
Jose Neyra <jose-neyra@outlook.com>
2022-10-24 14:38:19 -0400

    Updated RestTemplate, added save beer handler

--- src/main/java/com/joseneyra/sfmsbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/joseneyra/sfmsbreweryclient/web/client/BreweryClient.java
@@ -1,11 +1,13 @@
 package com.joseneyra.sfmsbreweryclient.web.client;
 
 import com.joseneyra.sfmsbreweryclient.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -13,19 +15,25 @@ import java.util.UUID;
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
-    private String apihost;     // Comes from the applications.properties
+    private String apiHost;     // Comes from the applications.properties
 
     private final RestTemplate restTemplate;
+
 
     public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
     public BeerDto getBeerById(UUID uuid) {
-        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
     }
 
-    public void setApihost(String apihost) {
-        this.apihost = apihost;
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apiHost + BEER_PATH_V1, beerDto);
+    }
+
+
+    public void setApiHost(String apiHost) {
+        this.apiHost = apiHost;
     }
 }
--- src/test/java/com/joseneyra/sfmsbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/joseneyra/sfmsbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,17 @@ class BreweryClientTest {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(dto);
+    }
+
+    @Test
+    void testSavedNewBeer() {
+        // Given
+        BeerDto beerDto = BeerDto.builder().beerName("Test Beer").build();
+
+        // When
+        URI uri = client.saveNewBeer(beerDto);
+
+        // Then
+        assertNotNull(uri);
     }
 }
