0bb6003ed3fbc5ec3888d06fbf6c0d914b2124ee
Jose Neyra <jose-neyra@outlook.com>
2022-10-25 15:02:49 -0400

    Updated RestTemplate, added Customer Handlers

--- src/main/java/com/joseneyra/sfmsbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/joseneyra/sfmsbreweryclient/web/client/BreweryClient.java
@@ -1,7 +1,7 @@
 package com.joseneyra.sfmsbreweryclient.web.client;
 
 import com.joseneyra.sfmsbreweryclient.web.model.BeerDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.joseneyra.sfmsbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -15,6 +15,8 @@ import java.util.UUID;
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
+
     private String apiHost;     // Comes from the applications.properties
 
     private final RestTemplate restTemplate;
@@ -25,11 +27,35 @@ public class BreweryClient {
     }
 
     public BeerDto getBeerById(UUID uuid) {
-        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid, BeerDto.class);
     }
 
     public URI saveNewBeer(BeerDto beerDto) {
         return restTemplate.postForLocation(apiHost + BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID uuid, BeerDto beerDto) {
+        restTemplate.put(apiHost + BEER_PATH_V1 + uuid, beerDto);
+    }
+
+    public void deleteBeer(UUID uuid) {
+        restTemplate.delete(apiHost + BEER_PATH_V1 + uuid);
+    }
+
+    public CustomerDto getCustomerById(UUID uuid) {
+        return restTemplate.getForObject(apiHost + CUSTOMER_PATH_V1 + uuid, CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apiHost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID uuid, CustomerDto customerDto) {
+        restTemplate.put(apiHost + CUSTOMER_PATH_V1 + uuid, customerDto);
+    }
+
+    public void deleteCustomer(UUID uuid) {
+        restTemplate.delete(apiHost + CUSTOMER_PATH_V1 + "/" + uuid);
     }
 
 
--- src/test/java/com/joseneyra/sfmsbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/joseneyra/sfmsbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package com.joseneyra.sfmsbreweryclient.web.client;
 
 import com.joseneyra.sfmsbreweryclient.web.model.BeerDto;
+import com.joseneyra.sfmsbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,5 +34,54 @@ class BreweryClientTest {
 
         // Then
         assertNotNull(uri);
+    }
+
+    @Test
+    void testUpdateBeer() {
+        // Given
+        BeerDto beerDto = BeerDto.builder().beerName("Test Beer").build();
+
+        // When
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void testDeleteBeer() {
+        // When
+        client.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        // Given
+        CustomerDto customerDto = CustomerDto.builder().name("Test Customer").build();
+
+        // When
+        URI uri = client.saveNewCustomer(customerDto);
+
+        // Then
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateCustomer() {
+        // Given
+        CustomerDto customerDto = CustomerDto.builder().name("Test Customer").build();
+
+        // When
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteCustomer() {
+        // When
+        client.deleteCustomer(UUID.randomUUID());
     }
 }
